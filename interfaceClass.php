38cae47b03d27e5b5d03439823f795c48f893b68
Tarun Kumar <tarun.kumar@softobiz.com>
2025-02-13 12:54:44 +0530

    Interface Added

---
+++ interfaceClass.php
@@ -0,0 +1,19 @@
+<?php
+///////// Interface //////////
+interface A{
+    function test();
+}
+
+interface X{
+    function hello();
+}
+
+class B implements A, X{
+    function test(){
+        //////////// Write your own logic ////////
+    }
+    function hello(){
+        //////////// Write your own logic ////////
+    }
+}
+?>

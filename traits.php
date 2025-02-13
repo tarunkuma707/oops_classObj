6d93cdd69c22c3a4a4b2cdd2500886d6bee7387d
Tarun Kumar <tarun.kumar@softobiz.com>
2025-02-13 13:33:46 +0530

    Alias of Function in traits if same function exists in multiple traits

--- traits.php
+++ traits.php
@@ -2,23 +2,30 @@
 
 trait A {
     function fun1(){
-        echo "Func1";
+        echo "A:Func1 A";
     }
 }
 
 trait B {
+    function fun1(){
+        echo "<br/>B:Func1 B";
+    }
     function fun2(){
-        echo "Func2";
+        echo "<br/>B:Func2";
     }
 }
 
 Class Class1{
-    use A,B;
+    use A,B{
+        A::fun1 insteadOf B;
+        B::fun1 as fun1B;////////// Alias of Function in traits ///
+    }
     public function func3(){
         echo "<br/>Class3: Func3";
     }
 }
 $obj =  new Class1();
 $obj->fun1();
+$obj->fun1B();
 $obj->fun2();
 $obj->func3();

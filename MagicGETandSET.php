ee01affe9490c7e148cb931badd795e38075678a
Tarun Kumar <tarun.kumar@softobiz.com>
2025-02-14 15:54:32 +0530

    Get and Set Magic Methods added

---
+++ MagicGETandSET.php
@@ -0,0 +1,26 @@
+<?php
+class demo{
+    private $data = [];
+
+    public function __set($name,$value){
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        if(!array_key_exists($name,$this->data)){
+            return null;
+        }
+        return $this->data[$name];
+    }
+
+    public function getData(){
+        return $this->data;
+    }
+}
+
+$example = new demo();
+echo $example->name=15;
+print_r($example->getData());
+
+?>

7c9d324b96a60b14c44593e90501639c55e7d272
Tarun Kumar <tarun.kumar@softobiz.com>
2025-02-14 10:50:27 +0530

    New Echo file

--- namespace/testing.php
+++ namespace/testing.php
@@ -3,7 +3,7 @@ namespace test{
     class Products{
         public function __construct()
         {
-            echo "This is testing page class";        
+            echo "This is testing page class";
         }
         function wow(){
             echo "WOW from Test.<br/>";
